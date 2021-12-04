4e9942296cb90cc2ff4f2e679f7d27ea8e5c686f
savalone47 <15yn450@esisalama.org>
2021-12-04 10:36:08 +0200

    laravel Inertia

---
+++ resources/js/Pages/Users.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-4xl font-bold">Users</h1>
+
+  <Nav />
+</template>
+
+<script>
+import Nav from '../Shared/Nav';
+
+export default {
+  components: { Nav },
+};
+</script>
